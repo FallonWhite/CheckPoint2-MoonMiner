a3e23ddc5f7c38472582efada9798d92ff0b766e
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-07-02 12:15:27 -0600

    functions

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 let cheese = 0
+
+function mineMoon() {
+  cheese += 1
+  update(cheese)
+}
+
+function update(cheese) {
+  document.getElementById("displayCheese").innerText = cheese.toString()
+}
