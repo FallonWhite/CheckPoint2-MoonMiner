8122c874e601c2b0423ecfbad95dd35c2ae16a4d
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-07-02 13:53:52 -0600

    added upgrades

--- app.js
+++ app.js
@@ -8,3 +8,37 @@ function mineMoon() {
 function update(cheese) {
   document.getElementById("displayCheese").innerText = cheese.toString()
 }
+
+let clickUpgrades = {
+  pickaxes: {
+    price: 1,
+    quantity: 0,
+    multiplier: 1
+  }
+
+  drill: {
+    price: 4,
+    quantity: 0,
+    multiplier: 4
+  }
+
+  explosives: {
+    price: 2,
+    quantity: 0,
+    multiplier: 3
+  }
+
+  excavators: {
+    price: 3,
+    quantity: 0,
+    multiplier: 5
+  }
+};
+
+let automaticUpgrades = {
+  rovers: {
+    price: 600,
+    quantity: 0,
+    multiplier: 20
+  }
+};
