6211c9ff440fe4718b2db39e80546c7a971786b4
FallonWhite <79472288+FallonWhite@users.noreply.github.com>
2021-07-02 20:46:07 -0600

    all

--- app.js
+++ app.js
@@ -13,46 +13,40 @@ let clickUpgrades = {
   pickaxes: {
     price: 100,
     quantity: 0,
-    multiplier: 1,
-    btn btn- primary
-},
+    multiplier: 2
+  }
 
   drills: {
     price: 125,
     quantity: 0,
-    multiplier: 2,
-    btn btn - secondary
+    multiplier: 3
   },
 
-explosives: {
-  price: 250,
+  explosives: {
+    price: 250,
     quantity: 0,
-      multiplier: 4,
-        btn btn - success
+    multiplier: 4
+  },
+
+  excavators: {
+    price: 1000,
+    quantity: 0,
+    multiplier: 6
+  }
 },
 
-excavators: {
-  price: 1000,
-    quantity: 0,
-      multiplier: 6,
-        btn btn - info
-}
-};
-
-let automaticUpgrades = {
-  lasers: {
-    price: 2000,
-    quantity: 0,
-    multiplier: 20,
-    btn btn- warning
-}
-};
+  let automaticUpgrades = {
+    lasers: {
+      price: 2000,
+      quantity: 0,
+      multiplier: 10
+    }
+  },
 
 function checkout() {
   cart = []
   drawTotal()
   drawCart()
 }
-
 
 
